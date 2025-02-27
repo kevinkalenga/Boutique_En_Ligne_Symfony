ee05ef55ce07049d0222f51c1b3c01d7c16a7e0b
kevin kalenga <kevinkalenga10@gmail.com>
2025-02-27 02:11:59 +0100

    Sauvegarder les informations du formulaire en base de donnée

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,17 +3,29 @@
 namespace App\Controller;
 
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(RegisterUserType::class);
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+
 
         return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView()
