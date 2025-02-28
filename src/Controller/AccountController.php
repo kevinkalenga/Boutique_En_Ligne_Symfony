b5ac3e564a4a0673a8bd7519639f91c6a086246d
kevin kalenga <kevinkalenga10@gmail.com>
2025-02-28 02:52:04 +0100

    Modification du mot de passe de l'utilisateur

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class AccountController extends AbstractController
 {
@@ -12,5 +16,26 @@ final class AccountController extends AbstractController
     public function index(): Response
     {
         return $this->render('account/index.html.twig');
+    }
+
+    #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUserType::class, $user, [
+            'passwordHasher' => $passwordHasher
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+        }
+
+
+        return $this->render('account/password.html.twig', [
+            'modifyPwd' => $form->createView()
+        ]);
     }
 }
