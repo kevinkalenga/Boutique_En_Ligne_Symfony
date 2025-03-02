606598947bf68f48ddc7a17975488afdd3b331e7
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-02 16:17:59 +0100

    Création du panier et de notre classe Cart()

---
+++ src/Classe/Cart.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Classe;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Cart
+{
+    // Création de constructeur et injection de dependance permettant d'aller chercher les sessions
+    public function __construct(private RequestStack $requestStack) {}
+
+    // fonction permettant l'ajout du produit au panier
+    public function add($product)
+    {
+        // appele de la session Cart de symfony
+        $cart = $this->requestStack->getSession()->get('cart');
+        // dd($session);
+
+        // Si il y a deja le produit ds le panier
+        if (isset($cart[$product->getId()])) {
+
+            $cart[$product->getId()] = [
+                'object' => $product,
+                'qty' => $cart[$product->getId()]['qty'] + 1
+            ];
+        } else {
+
+            $cart[$product->getId()] = [
+                'object' => $product,
+                'qty' => 1
+            ];
+        }
+
+
+        // Créer ma session Cart
+        $this->requestStack->getSession()->set('cart', $cart);
+    }
+
+    // fonction permettant de retourner le panier en cours
+    public function getCart()
+    {
+        return $this->requestStack->getSession()->get('cart');
+    }
+}
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,12 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Classe\Cart;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 final class CartController extends AbstractController
 {
     #[Route('/mon-panier', name: 'app_cart')]
-    public function index(): Response
+    public function index(Cart $cart): Response
     {
-        return $this->render('cart/index.html.twig');
+
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart->getCart(),
+
+        ]);
+    }
+
+    // La route permettant à ajouter un produit dans le panier
+    #[Route('/cart/add/{id}', name: 'app_cart_add')]
+    public function add($id, Cart $cart, ProductRepository $productRepository, Request $request): Response
+    {
+        $product = $productRepository->findOneById($id);
+        $cart->add($product);
+
+        $this->addFlash(
+            type: 'success',
+            message: "Produit correctement ajouté à votre panier."
+        );
+
+        return $this->redirectToRoute('app_product', [
+            'slug' => $product->getSlug()
+        ]);
     }
 }
